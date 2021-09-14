f63e8d35c623086cd223ca8f178c5e78a98233aa
Yushchenko Andrew <Andrew10122000@gmail.com>
2021-09-15 00:36:38 +0300

    Add simple auth

--- src/logs/logs.controller.ts
+++ src/logs/logs.controller.ts
@@ -1,4 +1,14 @@
-import { Body, Controller, HttpCode, Post } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  HttpCode,
+  Post,
+  Query,
+  Headers,
+  Logger,
+} from "@nestjs/common";
+import { PassValidate } from "lib/auth";
+import md5 from "lib/md5";
 import { MessageDto } from "./dto/message.dto";
 import { LogsService } from "./logs.service";
 
@@ -8,13 +18,23 @@ export class LogsController {
 
   @Post("/info")
   @HttpCode(204)
-  postInfo(@Body() body: MessageDto): void {
+  postInfo(
+    @Headers("X-Custom-Header") salt = "",
+    @Query("key") key = "",
+    @Body() body: MessageDto,
+  ): void {
+    if (!PassValidate(md5(salt + process.env.KEY), key)) return;
     return this.logsService.logMessage("info", body);
   }
 
   @Post("/alert")
   @HttpCode(204)
-  postAlert(@Body() body: MessageDto): void {
+  postAlert(
+    @Headers("X-Custom-Header") salt: string = "",
+    @Query("key") key: string = "",
+    @Body() body: MessageDto,
+  ): void {
+    if (!PassValidate(md5(salt + process.env.BOT_KEY), key)) return;
     return this.logsService.logMessage("alert", body);
   }
 }
